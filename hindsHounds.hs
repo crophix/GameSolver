c95b02a313d7eca86d66d3457f01ec245a4cdb17
crophix <dleblanc612@gmail.com>
2014-03-09 11:18:18 -0700

    finished ttt solver, refining hinds and hounds

--- hindsHounds.hs
+++ hindsHounds.hs
@@ -8,32 +8,93 @@ import Data.List
 import Data.Either
 import AsciiPic
 
-type Piece      = (Int, Int)
+type Position   = (Int, Int)
 type Moves      = [(Int, Int)]
-data Symbol     = H | F deriving (Eq, Show)
-type Square     = Either (Int, Int) Symbol
-type Board      = [[Square]]
-data GameState  = Game { board  :: Board,
+data Symbol     = Empty | H | F deriving (Eq, Show)
+data FoxHounds  = Game { board  :: [(Position,Symbol)],
                          onMove :: Symbol,
-                         hounds :: [Piece],
-                         fox    :: Piece}
+                         hounds :: [Position],
+                         fox    :: Position}
 
-foxMoves :: Moves
-foxMoves  = [(-1,-1),(1,-1),(-1,1),(1,1)]
-
-houndMoves :: Moves
+foxMoves   :: Moves --Diagonal in any direction
+foxMoves    = [(-1,-1),(1,-1),(-1,1),(1,1)]
+houndMoves :: Moves --Diagonal forward only
 houndMoves  = [(1,-1),(1,1)]
 
-moves         :: Piece -> Moves -> [(Int, Int)]
-moves (a,b) ms = [(a+x, b+y) | (x,y) <- ms, a+x > 0,b+y > 0]
+moves           :: Position -> [Position] -> Moves -> [(Int, Int)]
+moves (a,b) o ms = [(a+x, b+y) | (x,y) <- ms, a+x > 0 && a + x < 9,b+y > 0 && b+y < 9, not ((a+x,b+y) `elem` o)]
 
-scoreGame  :: GameState -> Maybe Int
-scoreGame g | null moves (fox g) foxMoves = Just p
-            | fst (fox g) == 1            = Just -p
-            | otherwise                   = Nothing
+scoreGame  :: FoxHounds -> Int
+scoreGame g | null (moves (fox g) (hounds g) foxMoves) =  p
+            | fst (fox g) == 1                         = -p
+            | otherwise                                =  0
               where p | onMove g == H =  1
                       | otherwise     = -1
 
-applyMove  :: GameState -> Piece -> Piece -> GameState
-applyMove game start end
-            = 
+gameOver  :: FoxHounds -> Bool
+gameOver g = null (moves (fox g) (hounds g) foxMoves) || fst (fox g) == 1
+
+switchPlayer  :: Symbol -> Symbol
+switchPlayer p | p == H    = F
+               | otherwise = H
+
+applyMove  :: FoxHounds -> Position -> Position -> FoxHounds
+applyMove g start end
+            = Game (map (clear . place) (board g)) (switchPlayer $ onMove g) h f
+              where clear a | fst a == start = (start,Empty)
+                            | otherwise      = a
+                    place a | fst a == end   = (end, onMove g)
+                            | otherwise      = a
+                    h | onMove g == H = end : [x | x <- hounds g, x /= start]
+                      | otherwise     = hounds g
+                    f | onMove g == F = end
+                      | otherwise     = fox g
+ 
+allMoves  :: FoxHounds -> [FoxHounds]
+allMoves g | onMove g == H = [applyMove g s e | s <- hounds g, e <- moves s [fox g] houndMoves]
+           | otherwise     = [applyMove g (fox g) e | e <- moves (fox g) (hounds g) foxMoves]
+
+negamax  :: FoxHounds -> Int
+negamax g | result /= 0 = result
+          | otherwise   = maximum (map (negate . negamax) (allMoves g))
+            where result = scoreGame g
+
+
+emptyBoard :: [(Position, Symbol)]
+emptyBoard  = [((a,b),Empty) | a <- [1..8], b <- [1..8]]
+
+startState :: FoxHounds
+startState  = Game emptyBoard F [(1,1),(1,3),(1,5),(1,7)] (8,6)
+
+fwin, hwin :: FoxHounds
+fwin  = Game emptyBoard F [(2,6),(2,4),(3,5),(4,6)] (1,1)
+hwin  = Game emptyBoard H [(8,6),(7,7),(6,8),(1,1)] (8,8)
+
+fillPos           :: [(Position,Symbol)] -> [Position] -> Symbol -> [(Position,Symbol)]
+fillPos b [] s     = b
+fillPos b (p:ps) s = fillPos (map (place p s) b) ps s
+                     where place p s (l,e) | l == p    = (l,s)
+                                           | otherwise = (l,e)
+
+
+
+picSquare          :: (Position,Symbol) -> Pic
+picSquare ((a,b),x) = align center [tope, align middle [edge, item]]
+                      where edge = text ["|"]
+                            tope = string "+---"
+                            item = string (r x)
+                            r Empty | even (a+b) = "   "
+                                    | odd  (a+b) = "###"
+                            r t                  = " " ++ show x ++ " "
+
+picBoard   :: [(Position,Symbol)] -> Pic
+picBoard ps = align center ([picRow x | x <- splitEvery 8 ps] ++ 
+                            [string "+---+---+---+---+---+---+---+---+"])
+
+picRow ps = align middle ((map picSquare ps) ++ [text ["+","|"]])
+
+splitEvery     :: Int -> [a] -> [[a]]
+splitEvery i ls = map (take i) (build (splitter ls))
+                  where splitter [] _ n = n
+                        splitter l c n  = l `c` splitter (drop i l) c n
+                        build g         = g (:) []
--- tttTree.hs
+++ tttTree.hs
@@ -4,11 +4,97 @@ Copyright (c) 2014 Daniel Leblanc
 
 -}
 
-import Negamax
+-- import Negamax
+import Data.List(transpose)
+import AsciiPic
 
-data Symbol    =  Empty | X | O deriving (Eq, Show)
+data Symbol    = Empty Int | X | O deriving (Eq, Show)
 data TicTacToe = Game { board :: [[Symbol]], onMove :: Symbol }
-
+                 deriving Show
+{-
 instance Node TicTacToe where
-    nextMoves a = [a]
-    scoreGame _ = 1
+    nextMoves = applyMoves
+    scoreGame = gameEval
+-}
+
+switchPlayer  :: Symbol -> Symbol
+switchPlayer X = O
+switchPlayer O = X
+
+applyMoves  :: TicTacToe -> [TicTacToe]
+applyMoves g | gameOver g = []
+             | otherwise  =[makeMove g s | s <- moves g]
+
+moves  :: TicTacToe -> [Symbol]
+moves g = [x | x <- concat (board g), x /= X, x /= O]
+
+makeMove    :: TicTacToe -> Symbol -> TicTacToe
+makeMove g s = Game (map (map place) (board g)) p
+               where p = switchPlayer (onMove g)
+                     place b | b == s    = onMove g
+                             | otherwise = b
+
+gameOver  :: TicTacToe -> Bool
+gameOver g = (not $ null (wonGame g)) || tieGame g
+
+wonGame  :: TicTacToe -> [Symbol]
+wonGame g = [x | [x,y,z] <- lines, x == y && y == z]
+            where diag [[a,_,b],
+                        [_,c,_],
+                        [d,_,e]] = [[a,c,e],[b,c,d]]
+                  brd   = board g
+                  lines = brd ++ diag brd ++ transpose brd
+
+gameEval  :: TicTacToe -> Int
+gameEval g | tieGame g               =  0
+           | null winner             =  1
+           | head winner == onMove g =  3
+           | otherwise               = -3
+             where winner = wonGame g
+
+tieGame :: TicTacToe -> Bool
+tieGame  = null . moves
+
+initialState :: TicTacToe
+initialState  = Game [[Empty 1,Empty 2,Empty 3],
+                      [Empty 4,Empty 5,Empty 6],
+                      [Empty 7,Empty 8,Empty 9]] X
+testState1 :: TicTacToe
+testState1  = Game [[Empty 1,O      ,Empty 3],
+                    [Empty 4,X      ,Empty 6],
+                    [Empty 7,X      ,Empty 9]] O
+
+testState2 :: TicTacToe
+testState2  = Game [[      O, X,Empty 3],
+                    [Empty 4, O,Empty 6],
+                    [Empty 7, X,Empty 9]] X
+
+testState3 :: TicTacToe
+testState3  = Game [[      O, X,Empty 3],
+                    [Empty 4, O,Empty 6],
+                    [      X, X,Empty 9]] O
+
+testState4 :: TicTacToe
+testState4  = Game [[O,X,O],
+                    [O,X,X],
+                    [X,O,X]] O
+
+
+negamax  :: TicTacToe -> Int
+negamax g | gameOver g = gameEval g
+          | otherwise  = maximum (map (negate . negamax) (applyMoves g))
+
+picGame       :: TicTacToe -> Pic
+picGame g      = picBoard (board g)       
+
+picBoard      :: [[Symbol]] -> Pic
+picBoard board = align center (map (align middle) b)
+                 where b = map (map picSquare) board
+
+picSquare     :: Symbol -> Pic
+picSquare x    = align center [tope, align middle [edge, item,edge], tope]
+                 where edge = text ["|","|","|"]
+                       tope = string "+-----+"
+                       item = string ("  " ++ (r x) ++ "  ")
+                       r (Empty n) = show n
+                       r a         = show x
